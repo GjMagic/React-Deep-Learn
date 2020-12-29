dbffb0447e90420b7e5fc9b8ce9d87d0dcf7a200
gongjian <gongjian@zycfcinc.com>
2020-12-29 18:10:36 +0800

    feat: 创建createElement函数

---
+++ tiny-react/src/TinyReact/createElement.js
@@ -0,0 +1,19 @@
+export default function createElement (type, props, ...children) {
+  const childElement = [].concat(...children).reduce((result, child) => {
+    if (child !== false && child !== true && child !== null) {
+      if (child instanceof Object) {
+        result.push(child)
+      } else {
+        result.push(createElement('text', { textContent: child }))
+      }
+    }
+
+    return result
+  }, [])
+
+  return {
+    type,
+    props: Object.assign({ children: childElement }, props),
+    children: childElement,
+  }
+}
