00994a21f599e8569ea5dce530569b52aa805de9
gongjian <gongjian@zycfcinc.com>
2021-01-11 13:50:41 +0800

    feat: virtualDOM比对之节点类型不同

--- tiny-react/src/TinyReact/diff.js
+++ tiny-react/src/TinyReact/diff.js
@@ -1,6 +1,7 @@
 import mountElement from './mountElement'
 import updateTextNode from './updateTextNode'
 import updateNodeElement from './updateNodeElement'
+import createDOMElement from './createDOMElement'
 
 export default function diff (virtualDOM, container, oldDOM) {
   const oldVirtualDOM = oldDOM && oldDOM._virtualDOM
@@ -8,6 +9,9 @@ export default function diff (virtualDOM, container, oldDOM) {
   // 判断旧节点是否存在
   if (!oldDOM) {
     mountElement(virtualDOM, container)
+  } else if (virtualDOM.type !== oldVirtualDOM.type && typeof virtualDOM.type !== 'function') {
+    const newElement = createDOMElement(virtualDOM)
+    oldDOM.parentNode.replaceChild(newElement, oldDOM)
   } else if (oldVirtualDOM && oldVirtualDOM.type === virtualDOM.type) {
     if (virtualDOM.type === 'text') {
       // 更新文本内容
