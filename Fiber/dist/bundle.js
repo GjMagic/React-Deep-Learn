4c1193cb37275938f87fd4874d9e1dee3ed8eab2
gongjian <gongjian@zycfcinc.com>
2021-01-13 15:29:07 +0800

    feat: 初始化Fiber项目

---
+++ Fiber/dist/bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('from client js');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
