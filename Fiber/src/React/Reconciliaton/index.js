1d94e597a115a2a17c802e8a2908d345a80738fd
gongjian <gongjian@zycfcinc.com>
2021-02-03 15:16:53 +0800

    feat: 构建effects数组

--- Fiber/src/React/Reconciliaton/index.js
+++ Fiber/src/React/Reconciliaton/index.js
@@ -59,6 +59,18 @@ const executeTask = fiber => {
     return fiber.child
   }
 
+  let currentExecutelyFiber = fiber
+  while (currentExecutelyFiber.parent) {
+    currentExecutelyFiber.parent.effects = currentExecutelyFiber.parent.effects.concat(
+      currentExecutelyFiber.effects.concat([currentExecutelyFiber])
+    )
+    if (currentExecutelyFiber.sibling) {
+      return currentExecutelyFiber.sibling
+    }
+
+    currentExecutelyFiber = currentExecutelyFiber.parent
+  }
+
   console.log(fiber)
 }
 
